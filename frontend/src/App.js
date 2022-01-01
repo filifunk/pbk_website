aa9db4095147f25aa164c855a56f1040b42ff19f
filifunk <filifunk@hotmail.com>
2022-01-01 16:29:18 -0500

    got rid of excess files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
-import IgersMiami from './pages/Igersmiami'
 import Blog from './pages/Blog'
-import Privacy from './pages/Privacy'
-import Nav from './components/Nav'
 import Contact from './pages/Contact'
 import About from './pages/About'
 
@@ -32,7 +29,6 @@ function App() {
 	  <Switch>
 	    <Route exact path="/blog#!" component={Blog} />
 	    <Route exact path = "/" component={Home} />
-	    <Route exact path = "/Privacy" component={Privacy} />
 	    <Route exact path = "/Contact" component={Contact} />
 	    <Route exact path = "/About" component={About} />
 	  </Switch>
