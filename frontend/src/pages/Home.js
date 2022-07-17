996d9345c98cccf379841115308fd37cd41dcb1d
filifunk <filifunk@hotmail.com>
2022-07-17 14:47:17 -0400

    got rid of email form

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -15,8 +15,6 @@ return(
       <body className="App-body">
 	<img src={logo} className="App-logo" alt="logo" />
 	<h1>Coming Soon...</h1>
-	<h3>submit email for updates or not</h3>
-	<Form/>
 	<SocialFollow />
 	
 
