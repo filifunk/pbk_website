417c6dbb6a812c97010e78b7e4bc8deb8c1e71f1
filifunk <filifunk@hotmail.com>
2022-10-06 07:35:59 -0400

    updated with Dannys improvements

--- frontend/src/pages/Promotion22.js
+++ frontend/src/pages/Promotion22.js
@@ -48,13 +48,13 @@ function Promotion22() {
 				2. ELIGIBILITY: Open to residents of states and countries where the app is available and 18 years of age and older as of January 1st, 2022. Void in countries where the app is not available and where otherwise prohibited.<br/><br/>
 				3. LIMITATION OF LIABILITY: By entering this promotion, entrants waive all right to, and hold Pobblebonk, LLC harmless from, any claim, liability, loss, damage (including punitive, incidental, and consequential damages), or expense (including attorneys' fees) arising out of or in connection with participation in this promotion or the acceptance, use, or misuse of any prize. <br/><br/>
 				4. WINNER SELECTION: Winners will be determined as follows: <br/>
-        &emsp;(a) Each registered check-in during the Promotion Period by an individual Comedian will be counted as one point and tallied to result in the “Check-In Total”. <br/>
+        &emsp;(a) Each registered check-in during the Promotion Period by an individual Fan will be counted as one point and tallied to result in the “Check-In Total”. <br/>
 
-        &emsp;(b) Each unique mic check-in (a mic that was not previously checked-in by Comedian) during the Promotion Period by an individual Fan will be calculated as follows: (5% of Check-Total) * (unique mic check-ins) = “Unique Mic Bonus”<br/>
+        &emsp;(b) Each unique mic check-in (a mic that was not previously checked-in by the Fan) during the Promotion Period by an individual Fan will be calculated as follows: (5% of Check-Total) * (unique mic check-ins) = “Unique Mic Bonus”<br/>
 
         &emsp;(c) The Check-In Total will be added to the Unique Mic Bonus to equal the “Promotion Score”<br/>
 
-        &emsp;(d) The Fans with the top 2 Promotion Scores will be eligible for the prizes set forth below. <br/>
+        &emsp;(d) The Fans with the top 10 Promotion Scores will be eligible for the prizes set forth below. <br/>
 
         &emsp;(e) By way of example: <br/>
 
@@ -73,20 +73,29 @@ function Promotion22() {
 				&emsp;&emsp;&emsp;(B) Their Unique Mic Bonus is (5% of 10 Check-In Total) * (4 unique mic check-in) = 2.0.<br />  
 				&emsp;&emsp;&emsp;(C) Their Promotion Score is 10 + 2.0 = 12.0<br/><br/>
 				5. PRIZES:<br /> 
-					&emsp;1st Place – two pairs of tickets to see shows performed by comedians who are registered on the app (max ticket price of $20/ticket) <br/>
-					&emsp;2nd Place – one pair of tickets to see shows performed by comedians who are registered on the app (max ticket price of $20/ticket) <br/>
-					&emsp;3rd Place - one pair of tickets to see a show performed by comedians who are registered on the app (max ticket price of $20/ticket) <br/>
+					&emsp;1st Place – two pairs of tickets to see two different shows of the winners choosing performed by comedians who are registered on the app (max ticket price of $20/ticket) <br/>
+					&emsp;2nd Place – one pair of tickets to see one show of the winners choosing performed by comedians who are registered on the app (max ticket price of $20/ticket) <br/>
+					&emsp;3rd Place - one pair of tickets to see one show of the winners choosing performed by comedians who are registered on the app (max ticket price of $20/ticket) <br/>
 					&emsp;1st through 10th Place - A Pobblebonk T-shirt<br/><br/>
 
 				6. CHOICE OF LAW, FORUM, TERMS, AND PRIVACY: Entrant agrees that all matters arising out of or relating to this promotion are governed by, and construed in accordance with, the laws of Florida, without giving effect to any of its conflict of laws provisions thereof. Entrant further agrees that any legal suit, action, or proceeding arising out of or relating to this promotion shall be brought exclusively in the applicable federal or state courts located in Miami-Dade, Florida. This promotion is subject to and governed by the <a href="http://www.pobblebonklive.com/Tos">Terms of Service</a> and <a href="http://www.pobblebonklive.com/Pp">Privacy Policy</a>.<br/><br/> 
 
 
-				<h3>MIC SUBMISSION PRIZES</h3><br/>
+				<h3>MIC SUBMISSION</h3><br/>
 
-				1.  Comedian who submits the most mics (compared to other comedians) by the end of the year will get an "Everything" account.  This only applies if the comedian doesn't have an "Everything" account already. <br /><br />
-				2.  Comedians who submit the top 5 most checked-into mics will get a "Premium" account and a pack of stickers.  If a different comedian is the "Mic Contact" (as defined in the app) than the submitter, the comedian who was the Mic Contact when the mic achieved the most check-ins over the Promotion Period will also get the same prize. <br /><br />
-				3.  Fan who submits the most mics (compared to other fans) by the end of the year will get a pair of tickets to see a show performed by comedians who are registered on the app (max ticket price of $20/ticket) <br /><br />
-				4.  CHOICE OF LAW, FORUM, TERMS, AND PRIVACY: Entrant agrees that all matters arising out of or relating to this promotion are governed by, and construed in accordance with, the laws of Florida, without giving effect to any of its conflict of laws provisions thereof. Entrant further agrees that any legal suit, action, or proceeding arising out of or relating to this promotion shall be brought exclusively in the applicable federal or state courts located in Miami-Dade, Florida. This promotion is subject to and governed by the <a href="http://www.pobblebonklive.com/Tos">Terms of Service</a> and <a href="http://www.pobblebonklive.com/Pp">Privacy Policy.</a> <br /><br />
+
+				1. HOW TO ENTER: Beginning on January 1st, 2022 through December 31, 2022 (the “Promotion Period”), log-in to the Pobblebonk app using your Fan account, and begin checking-in to open-mics. <br/><br/>
+				2. ELIGIBILITY: Open to residents of states and countries where the app is available and 18 years of age and older as of January 1st, 2022. Void in countries where the app is not available and where otherwise prohibited.<br/><br/>
+				3. LIMITATION OF LIABILITY: By entering this promotion, entrants waive all right to, and hold Pobblebonk, LLC harmless from, any claim, liability, loss, damage (including punitive, incidental, and consequential damages), or expense (including attorneys' fees) arising out of or in connection with participation in this promotion or the acceptance, use, or misuse of any prize. <br/><br/>
+				4. WINNER SELECTION: Each unique mic submitted and accepted will be counted as one mic submission and all mics submitted will be tallied up to determine the submitters mic submission total <br/><br/>
+
+
+				5. PRIZES: <br/>
+
+				&emsp;(a)Comedian who submits the most unique mics (compared to other comedians) by the end of the year will get an "Everything" account.  This only applies if the comedian doesn't have an "Everything" account already. <br /><br />
+				&emsp;(b)Comedians who submit the top 5 most checked-into mics will get a "Premium" account and a pack of stickers.  If a different comedian is the "Mic Contact" (as defined in the app) than the submitter, the comedian who was the Mic Contact when the mic achieved the most check-ins over the Promotion Period will also get the same prize. <br /><br />
+				&emsp;(c)Fan who submits the most unique mics (compared to other fans) by the end of the year will get a pair of tickets to see a show performed by comedians who are registered on the app (max ticket price of $20/ticket) <br /><br />
+				6.  CHOICE OF LAW, FORUM, TERMS, AND PRIVACY: Entrant agrees that all matters arising out of or relating to this promotion are governed by, and construed in accordance with, the laws of Florida, without giving effect to any of its conflict of laws provisions thereof. Entrant further agrees that any legal suit, action, or proceeding arising out of or relating to this promotion shall be brought exclusively in the applicable federal or state courts located in Miami-Dade, Florida. This promotion is subject to and governed by the <a href="http://www.pobblebonklive.com/Tos">Terms of Service</a> and <a href="http://www.pobblebonklive.com/Pp">Privacy Policy.</a> <br /><br />
 
 			</p>
 		</body>
