3d9afd04f98c18b8b090b01b0af72bcef3bff012
filifunk <filifunk@hotmail.com>
2022-10-07 16:43:39 -0400

    last promotion

--- frontend/src/pages/Promotion22.js
+++ frontend/src/pages/Promotion22.js
@@ -30,9 +30,9 @@ function Promotion22() {
 				&emsp;&emsp;&emsp;(C) Their Promotion Score is 10 + 2.0 = 12.0 <br /><br/>
 	  
 				5. PRIZES: <br />
-				&emsp;&emsp;1st Place – (i) $250.00 cash prize, (ii) photoshoot with the founder of Pobblebonk, LLC (with a single framed print included), and (iii) will be named “Most Active Comedian in the World 2022” <br />
-				&emsp;&emsp;2nd Place – (i) $50.00 cash prize and a (ii) small customized trophy <br />
-				&emsp;&emsp;3rd Place – (i) $25.00 cash prize and a (ii) small customized trophy <br />
+				&emsp;&emsp;1st Place – (i) $400.00 cash prize, (ii) photoshoot with the founder of Pobblebonk, LLC (with a single framed print included), and (iii) will be named “Most Active Comedian in the World 2022” <br />
+				&emsp;&emsp;2nd Place – (i) $300.00 cash prize and a (ii) small customized trophy <br />
+				&emsp;&emsp;3rd Place – (i) $250.00 cash prize and a (ii) small customized trophy <br />
 				&emsp;&emsp;1st through 10th Place - A Pobblebonk T-shirt<br /><br />
 
 				
@@ -84,17 +84,19 @@ function Promotion22() {
 				<h3>MIC SUBMISSION</h3><br/>
 
 
-				1. HOW TO ENTER: Beginning on January 1st, 2022 through December 31, 2022 (the “Promotion Period”), log-in to the Pobblebonk app using your Fan account, and begin checking-in to open-mics. <br/><br/>
+				1. HOW TO ENTER: Beginning on January 1st, 2022 through December 31, 2022 (the “Promotion Period”), log-in to the Pobblebonk app using your Fan or Comedian account, and begin checking-in to open-mics. <br/><br/>
 				2. ELIGIBILITY: Open to residents of states and countries where the app is available and 18 years of age and older as of January 1st, 2022. Void in countries where the app is not available and where otherwise prohibited.<br/><br/>
 				3. LIMITATION OF LIABILITY: By entering this promotion, entrants waive all right to, and hold Pobblebonk, LLC harmless from, any claim, liability, loss, damage (including punitive, incidental, and consequential damages), or expense (including attorneys' fees) arising out of or in connection with participation in this promotion or the acceptance, use, or misuse of any prize. <br/><br/>
-				4. WINNER SELECTION: Each unique mic submitted and accepted will be counted as one mic submission and all mics submitted will be tallied up to determine the submitters mic submission total <br/><br/>
+				4. WINNER SELECTION: Each unique mic submitted and accepted will be counted as one mic submission and all mics submitted will be tallied up to determine the submitter's mic submission total <br/><br/>
 
 
 				5. PRIZES: <br/>
 
 				&emsp;(a)Comedian who submits the most unique mics (compared to other comedians) by the end of the year will get an "Everything" account.  This only applies if the comedian doesn't have an "Everything" account already. <br /><br />
 				&emsp;(b)Comedians who submit the top 5 most checked-into mics will get a "Premium" account and a pack of stickers.  If a different comedian is the "Mic Contact" (as defined in the app) than the submitter, the comedian who was the Mic Contact when the mic achieved the most check-ins over the Promotion Period will also get the same prize. <br /><br />
-				&emsp;(c)Fan who submits the most unique mics (compared to other fans) by the end of the year will get a pair of tickets to see a show performed by comedians who are registered on the app (max ticket price of $20/ticket) <br /><br />
+				&emsp;(c)Fan who submits the most unique mics (compared to other fans) by the end of the year will get a pair of tickets to see a show performed by comedians who are registered on the app (max ticket price of $20/ticket) 
+				&emsp;(d)All recipients of a Mic Submission prize will get a Pobblebonk T-shirt<br /><br />
+				
 				6.  CHOICE OF LAW, FORUM, TERMS, AND PRIVACY: Entrant agrees that all matters arising out of or relating to this promotion are governed by, and construed in accordance with, the laws of Florida, without giving effect to any of its conflict of laws provisions thereof. Entrant further agrees that any legal suit, action, or proceeding arising out of or relating to this promotion shall be brought exclusively in the applicable federal or state courts located in Miami-Dade, Florida. This promotion is subject to and governed by the <a href="http://www.pobblebonklive.com/Tos">Terms of Service</a> and <a href="http://www.pobblebonklive.com/Pp">Privacy Policy.</a> <br /><br />
 
 			</p>
